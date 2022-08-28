42d5b5c172c82cdb8d679c61503ca4f259b1661c
Sorin Adam <sorinadam@Sorins-MacBook-Air.local>
2022-08-28 02:03:25 +0200

    reset db

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 10; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 20;
     const camp = new Campground({
